20745077b0a1701b28739442851055f9ebabc5d0
Mark K Cowan <mark@battlesnake.co.uk>
2016-07-22 18:06:54 +0100

    module just exports requireDir now

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-module.exports = {
-	gen: require('./gen'),
-	query: require('./query'),
-	escape: require('./util/escape'),
-	indent: require('./util/indent'),
-	listjoin: require('./util/listjoin'),
-};
+module.exports = require('require-dir')();
